3565db4449990aca7581c41f79bdc1ce5ba58c4b
romwm <73566034+romwm@users.noreply.github.com>
2023-06-23 19:06:27 -0300

    Add files via upload

---
+++ Task que no son exitosas Román Abril/Ejemplo 3 Román  Abril/Ejemplo1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Net.Http;
+
+namespace Ejemplo1
+{
+    public partial class Form1 : Form
+    {
+        private string apiURL;
+        private HttpClient httpclient;
+        public Form1()
+        {
+            InitializeComponent();
+            apiURL = "https://localhost:7004";
+            httpclient = new HttpClient();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //Thread.Sleep(5000);
+            
+            gif.Visible = true;
+            //await Task.Delay(5000);
+            await Esperar();
+            var nombre = textBox1.Text;
+            try
+            {
+                var saludo = await DevolverSaludo(nombre);
+                MessageBox.Show(saludo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            gif.Visible = false;
+        }
+        private async Task Esperar()
+        { 
+            await Task.Delay(5000);
+        }
+        private async Task <string> DevolverSaludo(string nombre)
+        {
+            using (var respuesta=await httpclient.GetAsync($"{apiURL}/saludos/{nombre}"))
+            {
+                respuesta.EnsureSuccessStatusCode();
+                var saludo = await respuesta.Content.ReadAsStringAsync();
+                return saludo;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Task que no son exitosas Román Abril/Ejemplo 3 Román  Abril/WebApi/Controllers/SaludosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("Saludos")]
+    [ApiController]
+    public class SaludosController:ControllerBase
+    {
+        [HttpGet("{nombre}")]
+        public ActionResult<string> ObtenerSaludo(string nombre)
+        {
+            return $"Saludos, {nombre}!";
+        }
+    }
+}
